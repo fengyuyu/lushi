cecb259ed22a35f940ee5bd4993a1512e417f6d2
feng <865300649@qq.com>
2015-10-29 23:02:17 +0800

    lushi_10_29_23_02

--- Assets/CardLibMgr.cs
+++ Assets/CardLibMgr.cs
@@ -2,17 +2,42 @@
 using System.Collections;
 using System.Collections.Generic;
 
+using CommonStruct;
+
 public class CardLibMgr : MonoBehaviour {
 
-    public List<GameObject> listCardLib;
+    private static CardLibMgr _instance;
+
+    public static CardLibMgr GetInstance() {
+        if(_instance == null) {
+            _instance = GameObject.FindObjectOfType<CardLibMgr>();
+        }
+        return _instance;
+    }
+
+    private Dictionary<int, string> dicCardIndexName;
+    private Dictionary<string, CardInfo> dicCardNameInfo;
 
 	// Use this for initialization
 	void Start () {
-	
+        InitDic();
 	}
-	
-	// Update is called once per frame
-	void Update () {
-	
-	}
+
+    private void InitDic() { 
+        //初始化字典数据
+    }
+
+    public string GetCardName(int index) {
+        return dicCardIndexName[index];
+    }
+
+    public CardInfo GetCardInfo(int index) {
+        return dicCardNameInfo[dicCardIndexName[index]];
+    }
+
+    //overload
+    public CardInfo GetCardInfo(string name)
+    {
+        return dicCardNameInfo[name];
+    }
 }
---
+++ Assets/CommonSt.cs
@@ -0,0 +1,20 @@
+﻿
+namespace CommonStruct{
+
+    enum CardSkill { 
+        NoSkill, //无技能
+        Mock, //嘲讽
+        Charge, //冲锋
+    }
+
+    //卡片信息
+    public struct CardInfo {
+        string m_name;  //名字
+        string m_attack; //攻击
+        string m_blood; //血量
+        CardSkill[] m_skill; //技能
+        int m_crystalNum; //水晶数 
+    }
+
+}
+
